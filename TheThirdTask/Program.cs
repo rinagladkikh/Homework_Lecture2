7540d31eda297bc7177e4c22c326e2cd520b2199
rina_gladkikh <rina.gladkikh@gmail.com>
2023-03-05 21:59:55 +0300

    Дни недели

---
+++ TheThirdTask/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число, обозначающее день недели: ");
+int UserNumber = int. Parse(Console. ReadLine () !);
+
+if(UserNumber < 6 || UserNumber >1){
+    Console.WriteLine("Это будний день -> нет");
+ }
+  else if (UserNumber == 6 || UserNumber == 7) {
+    Console.WriteLine("Это выходной -> да");
+  }
+else if(UserNumber >7 || UserNumber <1 ){
+    Console.WriteLine("Это не день недели");
+}
+
+
+ы
