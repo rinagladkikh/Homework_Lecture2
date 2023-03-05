0d3d46afdc2cdaa1e4fd1fc26ae49431e017fc41
rina_gladkikh <rina.gladkikh@gmail.com>
2023-03-05 21:31:27 +0300

    The third digit

---
+++ TheSecondTask/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Write("Введи число: ");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+string UserString = Convert.ToString(UserNumber);
+
+if(UserString.Length > 2){
+    Console.WriteLine("Третья цифра этого числа -> " + UserString[2]);
+}
+else
+{
+    Console.WriteLine("-> третьей цифры нет");
+}
+
